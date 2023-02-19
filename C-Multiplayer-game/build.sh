cab0acc78cff5c85ea57929c6b5a07d5ea549d12
Idan Banani <idanb121@gmail.com>
2023-02-20 00:28:47 +0200

    Revert "Pacman"
    This reverts commit aacdde2c82ce7bb5d892f586ff9719eea407bdfb.

--- C-Multiplayer-game/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd server
-make clean
-make
-cd ..
-cd client
-make clean
-make
-cd ..
--- C-Multiplayer-game/pacman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd pacman
-for i in $(seq 3)
-do
-./bin/client_b 222 3771 a bbb < ~/in.txt &>/dev/null & 
-done
